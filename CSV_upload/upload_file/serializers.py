47f70d3eeed6649bc01ef8e64c44a0a22a230809
Taniya-Namdev <taniya.namdev@dianapps.com>
2024-12-12 15:14:07 +0530

    Initial Project setup

---
+++ CSV_upload/upload_file/serializers.py
@@ -0,0 +1,9 @@
+# csv_app/serializers.py
+
+from rest_framework import serializers
+from .models import File
+
+class FileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = File
+        fields = ['file']
